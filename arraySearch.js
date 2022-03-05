4260361dd32295039fd9a88bf7cdb6bbaa89dc26
ImrulEmon <50496408+ImrulEmon@users.noreply.github.com>
2022-03-05 23:14:45 +0600

    Delete arraySearch.js

--- arraySearch.js
+++
@@ -1,7 +0,0 @@
-const books = ["A Smarter Way to Learn JavaScript","JavaScript: The Definitive Guide","Head First JavaScript Programming: A Brain-Friendly Guide","Eloquent JavaScript, 3rd Edition","The Principles of Object-Oriented JavaScript","JavaScript: The Good Parts","Secrets of the JavaScript Ninja","Professional JavaScript for Web Developers","Javascript for Beginners","Effective JavaScript","You Don’t Know JS"];
-
-for(let book of books){
-    if(book.toLowerCase().includes("javascript")){
-        console.log(book);
-    }
-}
